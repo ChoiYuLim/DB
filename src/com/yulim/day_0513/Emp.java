a80d00afde730387108e217633088a010933afac
yulim <yulim13sky@naver.com>
2023-05-14 07:56:44 +0900

    feat: emp table class
    0513

---
+++ src/com/yulim/day_0513/Emp.java
@@ -0,0 +1,60 @@
+package com.yulim.day_0513;
+
+public class Emp {
+    private String ename;
+    private int empno;
+    private String job;
+    private String sal;
+    private String comm;
+
+    public Emp(String ename, int empno, String job, String sal, String comm) {
+        super();
+        this.ename = ename;
+        this.empno = empno;
+        this.job = job;
+        this.sal = sal;
+        this.comm = comm;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getSal() {
+        return sal;
+    }
+
+    public void setSal(String sal) {
+        this.sal = sal;
+    }
+
+    public String getComm() {
+        if(comm==null) return "0";
+        return comm;
+    }
+
+    public void setComm(String comm) {
+        this.comm = comm;
+    }
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+}
